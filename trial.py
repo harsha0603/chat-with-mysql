8c2d7f65034423b2d9651890796d7f8420b84b41
harsha0603 <harshamassss@gmail.com>
2025-03-18 15:16:41 +0530

    deleted trial

--- trial.py
+++
@@ -1,5 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key="AIzaSyCO3-gvFtWWY4tb883_-68F-Q1WVGqrie8")
-response = llm.invoke("Hello, how can I assist you today?")
-print(response.content)
